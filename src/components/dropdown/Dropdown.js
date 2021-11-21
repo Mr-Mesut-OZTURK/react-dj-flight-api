78eaff81673b8c16b8041c1722a2aed0c401c38a
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-11-21 17:53:16 +0300

    frontend | added dropdown component

---
+++ src/components/dropdown/Dropdown.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { 
+    Dropdown as Dropdown2,
+    DropdownToggle,
+    DropdownItem,
+    DropdownMenu
+ } from 'reactstrap'
+
+
+const Dropdown = ({ data, setUserType }) => {
+
+    const [active, setActive] = useState(false)
+
+    return (
+        <div className="d-flex justify-content-center p-5">
+            <Dropdown2 isOpen={active} toggle={() => setActive(!active)}>
+
+                <DropdownToggle caret>
+                    Dropdown
+                </DropdownToggle>
+
+                <DropdownMenu>
+                    {
+                        data.map((item, index) => (
+                            <DropdownItem 
+                            index={index}
+                            onClick={()=> setUserType(item)}
+                            >
+                                {item.type}
+                            </DropdownItem>
+                        ))
+                    }
+                </DropdownMenu>
+
+            </Dropdown2>
+        </div>
+    )
+}
+
+export default Dropdown
